b8fb425c44ed6fe1b34569e72a96230bbed4b1b2
Luis Barrueco <luis.barrueco@gmail.com>
2011-06-23 13:19:39 -0300

    Fixed bug #1: Every private attribute should be named accordingly

--- sugarcrm.py
+++ sugarcrm.py
@@ -16,13 +16,13 @@ class SugarInstance:
     """This is a connection to a SugarCRM server."""
     
     def __init__(self, url, username, password, modules, ldap_passwd, ldap_iv):
-        self.url = url
-        self.username = username
-        self.password = password
+        self._url = url
+        self._username = username
+        self._password = password
         
-        self.wsdl = SOAPpy.WSDL.Proxy(url)
-#        self.wsdl.soapproxy.config.dumpSOAPOut = 1
-#        self.wsdl.soapproxy.config.dumpSOAPIn = 1
+        self._wsdl = SOAPpy.WSDL.Proxy(url)
+#        self._wsdl.soapproxy.config.dumpSOAPOut = 1
+#        self._wsdl.soapproxy.config.dumpSOAPIn = 1
         
         if ldap_passwd == '' or ldap_iv == '':
             password_hash = hashlib.md5()
@@ -41,11 +41,11 @@ class SugarInstance:
                 ciphertext_hex += "%02x" % ord(byte)
             digest = ciphertext_hex
         
-        result = self.wsdl.login({'user_name': username,
+        result = self._wsdl.login({'user_name': username,
                             'password': digest,
                             'version': '0.1'}, 'Sugar.py')
         
-        self.session = result['id']
+        self._session = result['id']
         
         self.modules = {}
         for module in modules:
@@ -58,21 +58,21 @@ class SugarInstance:
         rel['module2'] = secondary.module.module_name
         rel['module2_id'] = secondary['id']
         
-        self.wsdl.set_relationship(self.session, rel)
+        self._wsdl.set_relationship(self.session, rel)
 
 
 class SugarModule:
     """Defines a SugarCRM module."""
     
     def __init__(self, instance, module_name):
-        self.module_name = module_name
-        self.instance = instance
+        self._module_name = module_name
+        self._instance = instance
         
         # Get the module fields through SugarCRM API.
-        result = self.instance.wsdl.get_module_fields(self.instance.session,
-                                                         self.module_name)
+        result = self._instance._wsdl.get_module_fields(self._instance._session,
+                                                         self._module_name)
 
-        self.fields = result['module_fields']
+        self._fields = result['module_fields']
 
 
     def search(self, query_str, start = 0, total = 20, fields = []):
@@ -85,8 +85,8 @@ class SugarModule:
         count = 0
         offset = 0
         while count < total:
-            result = self.instance.wsdl.get_entry_list(
-                            self.instance.session, self.module_name,
+            result = self._instance._wsdl.get_entry_list(
+                            self._instance._session, self._module_name,
                             query_str, '', start + offset, fields,
                             total - count, 0)
             if result['result_count'] == 0:
@@ -98,7 +98,7 @@ class SugarModule:
                     new_entry = SugarEntry(self)
                     
                     for attribute in result['entry_list'][i]['name_value_list']:
-                        new_entry.fields[attribute['name']] = attribute['value']
+                        new_entry._fields[attribute['name']] = attribute['value']
             
                     # SugarCRM seems broken, because it retrieves several copies
                     #  of the same contact for every opportunity related with
@@ -121,22 +121,22 @@ class SugarEntry:
         """Represents a new or an existing entry."""
 
         # Keep a reference to the parent module.
-        self.module = module
+        self._module = module
         
         # Keep a mapping 'field_name' => value for every valid field retrieved.
-        self.fields = {}
-        self.dirty_fields = []
+        self._fields = {}
+        self._dirty_fields = []
         
         # Make sure that the 'id' field is always defined.
-        if 'id' not in self.fields.keys():
-            self.fields['id'] = ''
+        if 'id' not in self._fields.keys():
+            self._fields['id'] = ''
     
 
     def __getitem__(self, field_name):
         """Value to return when self['field_name'] is used."""
-        if field_name in [item['name'] for item in self.module.fields]:
+        if field_name in [item['name'] for item in self._module._fields]:
             try:
-                return self.fields[field_name]
+                return self._fields[field_name]
             except KeyError:
                 if self['id'] == '':
                     # If this is a new entry, the 'id' field is yet undefined.
@@ -144,15 +144,15 @@ class SugarEntry:
                 else:
                     # Retrieve the field from the SugarCRM instance.
                     
-                    q_str = self.module.module_name.lower() + \
+                    q_str = self._module._module_name.lower() + \
                                 ".id='%s'" % self['id']
-                    res = self.module.instance.wsdl.get_entry_list(
-                            self.module.instance.session,
-                            self.module.module_name,
+                    res = self._module._instance._wsdl.get_entry_list(
+                            self._module._instance._session,
+                            self._module._module_name,
                             q_str, '', 0, [field_name], 1, 0)
                     for attribute in res['entry_list'][0]['name_value_list']:
                         if attribute['name'] == field_name:
-                            self.fields[attribute['name']] = attribute['value']
+                            self._fields[attribute['name']] = attribute['value']
                             return attribute['value']
 
         else:
@@ -160,10 +160,10 @@ class SugarEntry:
 
 
     def __setitem__(self, field_name, value):
-        if field_name in [item['name'] for item in self.module.fields]:
-            self.fields[field_name] = value
-            if field_name not in self.dirty_fields:
-                self.dirty_fields.append(field_name)
+        if field_name in [item['name'] for item in self._module._fields]:
+            self._fields[field_name] = value
+            if field_name not in self._dirty_fields:
+                self._dirty_fields.append(field_name)
         else:
             raise AttributeError
 
@@ -175,11 +175,11 @@ class SugarEntry:
         # If 'id' wasn't blank, it's added to the list of dirty fields; this
         # way the entry will be updated in the SugarCRM instance.
         if self['id'] != '':
-            self.dirty_fields.append('id')
+            self._dirty_fields.append('id')
         
         # nvl is the name_value_list, which has the list of attributes.
         nvl = []
-        for field in set(self.dirty_fields):
+        for field in set(self._dirty_fields):
             # Define an individual name_value record.
             nv = {}
             nv['name'] = field
@@ -187,17 +187,17 @@ class SugarEntry:
             nvl.append(nv)
         
         # Use the API's set_entry to update the entry in SugarCRM.
-        result = self.module.instance.wsdl.set_entry(
-                                                self.module.instance.session,
-                                                self.module.module_name, nvl)
-        self.fields['id'] = result['id']
-        self.dirty_fields = []
+        result = self._module._instance._wsdl.set_entry(
+                                                self._module._instance._session,
+                                                self._module._module_name, nvl)
+        self._fields['id'] = result['id']
+        self._dirty_fields = []
 
         return True
 
 
     def relate(self, related):
-        self.module.instance.relate(self, related)
+        self._module._instance.relate(self, related)
 
 
 class QueryList():
@@ -236,7 +236,7 @@ class QueryList():
 
 
 
-    def build_query(self, **query):
+    def _build_query(self, **query):
         # Build the API query string
         q_str = ''
         for key in query.keys():
@@ -249,7 +249,7 @@ class QueryList():
             if key_field.endswith('_c'):
                 if_cstm = '_cstm'
 
-            field = self._module.module_name.lower() + if_cstm + '.' + key_field
+            field = self._module._module_name.lower() + if_cstm + '.' + key_field
 
             if key_oper == 'exact':
                 q_str += '%s = "%s"' % (field, query[key])
@@ -277,18 +277,18 @@ class QueryList():
 
     def filter(self, **query):
         if self._query != '':
-            query = '(%s) AND (%s)' % (self._query, self.build_query(**query))
+            query = '(%s) AND (%s)' % (self._query, self._build_query(**query))
         else:
-            query = self.build_query(**query)
+            query = self._build_query(**query)
 
         return QueryList(self._module, query)
 
 
     def exclude(self, **query):
         if self._query != '':
-            query = '(%s) AND NOT (%s)' % (self._query, self.build_query(**query))
+            query = '(%s) AND NOT (%s)' % (self._query, self._build_query(**query))
         else:
-            query = 'NOT (%s)' % self.build_query(**query)
+            query = 'NOT (%s)' % self._build_query(**query)
 
         return QueryList(self._module, query)
 
